8b5c527c43102db11b8c7eed3bcf6bf70f312c42
john clampet <jsclampet@gmail.com>
2023-04-30 15:48:28 -0500

    last commit, added property bindings and ngfor to dynamically outline and generate cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    { 
+      title: 'Neat Tree',
+      imageUrl: 'assets/img/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today'
+    },
+    { 
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/img/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'Here is a picture of a snowy mountain'
+    },
+    { 
+      title: 'Mountain Biking',
+      imageUrl: 'assets/img/biking.jpeg',
+      username: 'biking1222',
+      content: 'I did some biking today'
+    }
+  ]
+
+  ngOnInit() {}
 }
